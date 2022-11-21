44ae27409cf9f301a86ce6b6cdf546ef517abd3f
Pako Cárdenas <pakorro2@gmail.com>
2022-11-20 20:30:06 -0600

    changes header, footer, card, home, and db

--- tiendacursos/src/components/Footer.jsx
+++ tiendacursos/src/components/Footer.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='container-fluid' id="footer">
-      <h1>footer</h1>
-    </footer>
+    <footer className="bg-light text-center text-lg-start mt-5">
+      <div className="text-center p-3" style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+        © 2022 Copyright:
+        <a className="text-dark" href="#"> Tech Courses</a>
+      </div>
+    </footer >
   )
 }
 
--- tiendacursos/src/components/Header.jsx
+++ tiendacursos/src/components/Header.jsx
@@ -3,7 +3,14 @@ import React from 'react';
 const Header = () => {
   return (
     <div>
-      <h1>header</h1>
+      <div style={{ background: 'url(https://bootstrapious.com/i/snippets/sn-static-header/background.jpg)' }} className="jumbotron bg-cover text-white">
+        <div className="container py-5 text-center">
+          <h1 className="display-4 font-weight-bold">Tech Courses</h1>
+          <p className="font-italic mb-0">Aprende cuando quieras y donde quieras.</p>
+          <br />
+          <a href="#" role="button" className="btn btn-primary px-5">See All Courses</a>
+        </div>
+      </div>
     </div>
   )
 }
