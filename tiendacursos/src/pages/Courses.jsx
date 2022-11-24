ad7b69aa1d3a998163f2d1714a01d18662ce7e75
Pako Cárdenas <pakorro2@gmail.com>
2022-11-24 16:21:41 -0600

    add pages about contact courses

---
+++ tiendacursos/src/pages/Courses.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Courses = () => {
+  return (
+    <div>
+      <h1>Courses</h1>
+      <Link to='/'>Return Home</Link>
+    </div>
+  )
+}
+
+export default Courses
