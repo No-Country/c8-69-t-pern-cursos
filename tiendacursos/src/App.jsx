a30d0d524a3237321fb15f9709a2a6f6a2bdb682
Alan <alandelleore@gmail.com>
2022-11-24 19:26:51 -0300

    subiendo detail

--- tiendacursos/src/App.jsx
+++ tiendacursos/src/App.jsx
@@ -1,20 +1,19 @@
-import { Route, HashRouter, Routes } from 'react-router-dom'
-import './App.css'
-import Home from './pages/Home'
-import Login from './pages/Login'
+import { Route, HashRouter, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
-
   return (
     <div className="App">
       <HashRouter>
         <Routes>
-          <Route path='/login' element={<Login />} />
-          <Route path='/' element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </HashRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
