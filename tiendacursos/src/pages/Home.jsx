ab688a9d82c8803b4abab95e8cde609e91398c2a
Pako Cárdenas <pakorro2@gmail.com>
2022-11-25 17:10:12 -0600

    add routes coursesdetail create courselist component

--- tiendacursos/src/pages/Home.jsx
+++ tiendacursos/src/pages/Home.jsx
@@ -1,20 +1,16 @@
 import React from 'react'
 import Card from '../components/Card'
-import Footer from '../components/Footer'
 import Header from '../components/Header'
-import NavBar from '../components/NavBar'
 
 
 const Home = () => {
 
   return (
     <div className='container-xl '>
-      <NavBar />
       <Header />
-      <main class="row">
+      <main className="row">
         <Card />
       </main>
-      <Footer />
     </div>
   )
 }
