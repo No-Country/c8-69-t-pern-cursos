0526fd1fbd7162f5acb86608a457e620533dc270
luisfer194 <luissfer194@gmail.com>
2022-11-25 18:24:03 -0500

    Actualizacion 1.0

--- tiendacursos/src/pages/Home.jsx
+++ tiendacursos/src/pages/Home.jsx
@@ -3,18 +3,20 @@ import Card from '../components/Card'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 import NavBar from '../components/NavBar'
+import CoursDetail from '../components/CourseDetail'
 
 
 const Home = () => {
 
   return (
     <div className='container-xl '>
-      <NavBar />
+    <NavBar />
       <Header />
       <main class="row">
         <Card />
       </main>
-      <Footer />
+      <Footer />       
+      <CoursDetail />
     </div>
   )
 }
