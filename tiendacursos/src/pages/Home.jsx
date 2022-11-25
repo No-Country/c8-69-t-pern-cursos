2c2b58d053c41a107d8a86f8017f8d1030531cd2
luisfer194 <luissfer194@gmail.com>
2022-11-25 18:43:15 -0500

    Actualizacion 1.1

--- tiendacursos/src/pages/Home.jsx
+++ tiendacursos/src/pages/Home.jsx
@@ -1,30 +1,21 @@
 import React from 'react'
 import Card from '../components/Card'
 import Header from '../components/Header'
-<<<<<<< HEAD
 import NavBar from '../components/NavBar'
 import CoursDetail from '../components/CourseDetail'
-=======
->>>>>>> frontend
 
 
 const Home = () => {
 
   return (
     <div className='container-xl '>
-<<<<<<< HEAD
     <NavBar />
-=======
->>>>>>> frontend
       <Header />
       <main className="row">
         <Card />
       </main>
-<<<<<<< HEAD
       <Footer />       
       <CoursDetail />
-=======
->>>>>>> frontend
     </div>
   )
 }
