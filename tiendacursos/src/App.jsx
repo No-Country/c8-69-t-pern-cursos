d0a27011d8879cd62686c1b782cb39c979cf68a5
Alan <alandelleore@gmail.com>
2022-11-26 15:29:14 -0300

    add header

--- tiendacursos/src/App.jsx
+++ tiendacursos/src/App.jsx
@@ -1,26 +1,25 @@
-import { Route, HashRouter, Routes } from 'react-router-dom'
-import './App.css'
-import About from './pages/About'
-import Contact from './pages/Contact'
-import Courses from './pages/Courses'
-import Home from './pages/Home'
-import Login from './pages/Login'
+import { Route, HashRouter, Routes } from "react-router-dom";
+import "./App.css";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Courses from "./pages/Courses";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
-
   return (
     <div className="App">
       <HashRouter>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/contact' element={<Contact />} />
-          <Route path='/courses' element={<Courses />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/courses" element={<Courses />} />
         </Routes>
       </HashRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
