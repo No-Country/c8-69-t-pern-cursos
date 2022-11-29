2fb5aa53797b7b00dbd52968b63745a735d03920
danieldrako <ls.ortizdaniel@gmail.com>
2022-11-29 15:22:43 -0600

    update app and readme

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
 //? Dependencies
 const express = require('express');
+const cors = require('cors')
 const db = require('./utils/database')
-
 //? Files
 const {port} = require('./config');
 //* Routes
 const userRouter = require('./users/users.router')
 const authRouter = require('./auth/auth.router')
 const categoryRouter = require('./categories/categories.router')
-const postRouter = require('./posts/posts.router')
+const courseRouter = require('./courses/courses.router')
+const videoRouter = require('./videos/videos.router')
 
 const initModels = require('./models/initModels')
 
@@ -46,7 +47,8 @@ app.get('/',(req, res) => {
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/categories', categoryRouter)
-app.use('/api/v1/posts', postRouter)
+app.use('/api/v1/courses', courseRouter)
+app.use('/api/v1/videos', videoRouter)
 
 app.listen(port, () => {
     console.log(`Server started at port ${port}`)
